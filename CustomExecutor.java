a3e6591fae5826bd8f162bb145ad6694e1e35a9c
gal <Galh2011@gmail.com>
2023-01-09 10:09:55 +0200

    add comments to the code

--- CustomExecutor.java
+++ CustomExecutor.java
@@ -1,9 +1,17 @@
 import java.util.concurrent.*;
 
 public class CustomExecutor {
+    // The ExecutorService that will execute the tasks
     private final ExecutorService Executor;
+    // A priority queue to store the tasks based on their priority (TaskType)
     private final PriorityBlockingQueue<Runnable> Priorities;
 
+    /**
+     * The constructor:
+     * * Get the number of available processors
+     * * Initialize the Executor with a thread pool of size [processors / 2, processors - 1]
+     * * The queue size is set to 300, and the threads will wait for 300 milliseconds before timing out
+     */
     public CustomExecutor() {
         int processors = Runtime.getRuntime().availableProcessors();
         Priorities = new PriorityBlockingQueue<>();
@@ -11,24 +19,47 @@ public class CustomExecutor {
                 300, TimeUnit.MILLISECONDS, Priorities);
     }
 
+    /**
+     * Submit a task with a given priority (TaskType)
+     *
+     * @param task
+     * @param <V>
+     * @return
+     */
     public <V> Task<V> submit(Task<V> task) {
         submitTask(task);
         return task;
     }
 
-
+    /**
+     * Create a new Task object with the given Callable and TaskType, and then submit it
+     *
+     * @param operation
+     * @param type
+     * @param <T>
+     * @return
+     */
     public <T> Task<T> submit(Callable<T> operation, TaskType type) {
         final Task<T> task = Task.createTask(operation, type);
         submitTask(task);
         return task;
     }
 
+    /**
+     * Submit the task to the Executor by wrapping it in a Future object
+     *
+     * @param task
+     */
     private void submitTask(Task<?> task) {
         final Future future = Executor.submit(task);
         task.setFuture(future);
     }
 
-
+    /**
+     * Get the maximum priority (TaskType) of the tasks currently in the queue
+     *
+     * @return
+     */
     public String getCurrentMax() {
         Task task = (Task) Priorities.peek();
         if (task != null) {
@@ -37,6 +68,9 @@ public class CustomExecutor {
         return "There is no task";
     }
 
+    /**
+     * Gracefully terminate the Executor
+     */
     public void gracefullyTerminate() {
         Executor.shutdown();
         try {
@@ -46,6 +80,5 @@ public class CustomExecutor {
         } catch (InterruptedException e) {
             System.out.println(e);
         }
-
     }
 }
--- Task.java
+++ Task.java
@@ -1,45 +1,101 @@
 import java.util.concurrent.*;
 
 public class Task<V> implements Comparable<Task<V>>, Callable<V> {
+    // The Future object representing the result of the task
     private Future<V> future;
+    // The operation that the task will execute
     private final Callable<V> operation;
+    // The type of the task
     private final TaskType Type;
 
+    // Constructor that takes a Callable and a TaskType
     public Task(Callable<V> task, TaskType type) {
         operation = task;
         Type = type;
     }
 
+    /**
+     * Constructor that takes only a Callable
+     *
+     * @param task
+     */
     public Task(Callable<V> task) {
         operation = task;
         Type = TaskType.OTHER;
     }
 
+    /**
+     * Factory method that creates and returns a new Task object
+     *
+     * @param task
+     * @param taskType
+     * @param <T>
+     * @return
+     */
     public static <T> Task<T> createTask(Callable<T> task, TaskType taskType) {
         return new Task<T>(task, taskType);
     }
 
+    /**
+     * Compares the types of two tasks
+     *
+     * @param t1 the object to be compared.
+     * @return
+     */
     @Override
     public int compareTo(Task t1) {
         return t1.Type.compareTo(Type);
     }
 
+    /**
+     * Returns the result of the task when it is executed with a timeout
+     *
+     * @param num
+     * @param timeUnit
+     * @return
+     * @throws InterruptedException
+     * @throws ExecutionException
+     * @throws TimeoutException
+     */
     public V get(long num, TimeUnit timeUnit) throws InterruptedException, ExecutionException, TimeoutException {
         return (V) future.get(num, timeUnit);
     }
 
+    /**
+     * Returns the result of the task when it is executed without a timeout
+     *
+     * @return
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
     public V get() throws InterruptedException, ExecutionException {
         return (V) future.get();
     }
 
+    /**
+     * Executes the operation that was passed to the Task constructor
+     *
+     * @return
+     * @throws Exception
+     */
     public V call() throws Exception {
         return this.operation.call();
     }
 
+    /**
+     * Sets the Future object representing the result of the task
+     *
+     * @param future
+     */
     public void setFuture(Future<V> future) {
         this.future = future;
     }
 
+    /**
+     * Returns the type of the task
+     *
+     * @return
+     */
     public TaskType getType() {
         return Type;
     }
